4c7a84678850a7889ab1358e734ecd5573a9fb4f
¨DevSeba78¨ <villarroelssebastian@gmail.com>
2021-12-05 20:36:49 -0300

    pug formulario no muestra listado si por id

--- contenedor.js
+++ contenedor.js
@@ -15,17 +15,40 @@ class Contenedor {
     }
     async getById(id){
         try {
-            let respuesta = "producto inexistente"
-            let contenido = await this.getProductos();
-            if(contenido.length > 0){
-                contenido.forEach(producto => {
-                    if(producto.id == id){
-                        respuesta = producto;
-                    }
+            let respuesta = 'El producto no existe'
+            let productos = await this.getProductos();
+            if(productos.length > 0){
+                productos.forEach(element => {
+                    element.id == id ? respuesta = element : respuesta;
                 });
-                return respuesta;
             }
+            return respuesta;
         } catch (error) {
+            throw new Error(error);
+        }
+    }
+    async save(producto){//guardar y devolver el id del producto
+        try{
+            let data = await this.getProductos();
+            let newId = await this.getNewId(data);
+            let newProducto = {id: newId, ...producto};
+            data.push(newProducto);
+            let content = JSON.stringify(data, null,2);
+            await fs.promises.writeFile(this.url, content);
+
+            return newProducto;
+           // await fs.writeFileSync(this.url, JSON.stringify(data));
+        }catch(error){
+            console.log(error);
+        }
+    }
+    async getNewId(productos){
+        try{
+            let resp = productos.reduce((max, producto) => {
+                return producto.id > max ? producto.id : max;
+            }, 0);
+            return resp + 1;
+        }catch(error){
             console.log(error);
         }
     }
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ let express = require('express');
 let app = express();
 let path = require('path');
 const PORT = 8080;
+let {Router} = express;
 let Contenedor = require('./contenedor')
 let contenedor = new Contenedor('./api/productos.txt');
+let router = new Router();
+//let pdr =[];
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -12,18 +15,48 @@ app.set('views', path.join(__dirname, './views', 'pug'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    console.log(req.query);
-    res.render('formularioPug.pug', req.query);
-})
-app.get('/productos', (req, res) => {
+    console.log("get raiz",req.query);
+    res.render('formularioPug');
+});
+router.get('/productos', (req, res) => {
     console.log(req.query);
     contenedor.getProductos()
-    res.render('formularioPug.pug', req.query);
-})
+        .then((producto)=>{
+            res.render('templatePug', {producto});
+            console.log("get",producto)
+    })
+        .catch((error)=>{
+            res.send(error);
+    })    
+});
+
+router.post('/productos', (req, res) => {
+    contenedor.save(req.body)
+    .then(producto => {
+        
+        console.log("producto guardado", producto);
+        
+        //res.render('templatePug', {producto});
+        res.redirect('/');
+       
+    }).catch(error => {
+        res.send(error);
+    });
+});
+
+router.get('/productos/:id', (req, res,next) => {
+    console.log('ID:', req.params.id)
+        contenedor.getById(req.params.id).then(producto => {
+            res.render('templatePug', {producto});
+            console.log("id", producto)
+        }).catch(error => {
+            res.send(error);
+        });
+});
 
 
 
-
+app.use('/api',router);
 app.listen(PORT, ()=>{
     console.log(`Server escuchando desde http://localhost:${PORT}`)
 })
